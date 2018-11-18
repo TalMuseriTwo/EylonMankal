232586e64f1db6fcf7ae1043ac90f7190ee45fdf
TalMuseriTwo <43849397+TalMuseriTwo@users.noreply.github.com>
2018-11-18 18:41:57 +0200

    Create dont-look-here.js

---
+++ s/e/c/r/e/t/dont-look-here.js
@@ -0,0 +1,27 @@
+// Don't just check what the password is! It's not fun that way!
+// There IS another way to discover what the password is.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function login() {
+  password = prompt ("אנא הזן סיסמה.");
+  if (password == ".") {
+    alert (".סיסמה נכונה. ברוך הבא, אילון");
+  } else {
+    alert (".סיסמה שגויה. אנא מחק את הזיכרון שלך כדי שתשכח שהאתר הזה קיים. תודה");
+  }
+}
